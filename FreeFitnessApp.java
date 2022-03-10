385cf620949fb5bad1bcd814d032baf3229ed492
sebstirs <98188443+sebstirs@users.noreply.github.com>
2022-03-10 15:15:16 -0500

    Add files via upload

---
+++ FreeFitnessApp.java
@@ -0,0 +1,22 @@
+public class FreeFitnessApp {
+
+	class workout {
+		
+		private int sets, reps, targetWeight;
+		
+		public void getSets(int inputSet) {
+			sets = inputSet;
+		}
+		public void getReps(int inputReps) {
+			sets = inputReps;
+		}
+		public void getTargetWeight(int inputTargetWeight) {
+			sets = inputTargetWeight;
+		}
+	
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
