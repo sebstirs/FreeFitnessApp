87d234f75e6890cd1331954566dea83e5148d60a
sebstirs <98188443+sebstirs@users.noreply.github.com>
2022-03-20 18:19:06 -0400

    Update FreeFitnessApp.java

--- FreeFitnessApp.java
+++ FreeFitnessApp.java
@@ -1,21 +1,6 @@
 public class FreeFitnessApp {
 
-	class input {
-		
-		private int sets, reps, targetWeight;
-		
-		public void getSets(int inputSet) {
-			sets = inputSet;
-		}
-		public void getReps(int inputReps) {
-			sets = inputReps;
-		}
-		public void getTargetWeight(int inputTargetWeight) {
-			sets = inputTargetWeight;
-		}
-	
-	}
-	class exercises{
+	class Excercise{
 		
 	}
 	class workouts{
