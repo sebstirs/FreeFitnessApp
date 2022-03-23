2225accb6bbc5e1fd3296bd2a12fe703fe10246a
sebstirs <98188443+sebstirs@users.noreply.github.com>
2022-03-23 12:50:17 -0400

    Update Excercise.java

--- Excercise.java
+++ Excercise.java
@@ -9,16 +9,24 @@
  *
  * @author hoangdao
  */
+import java.util.*;
+
 public class Excercise {
-    private final int defaultvalue=0; // we can change the default value here
+    Scanner myScanner = new Scanner(System.in);
+    private final int defaultvalue=1; // we can change the default value here
     private final double defaultweight=5;// default value for weight=5lbs
+    private String excerciseName;
     private int sets;
     private int reps;
     private double weight; //weight calculated in lbs
-    public Excercise(int s, int r, double w){
+    public Excercise(){
+
+    }
+    public Excercise(int s, int r, double w, String n){
         this.sets=s;
         this.reps=r;
         this.weight=w;
+        this.excerciseName=n;
     }
     public Excercise(double w){
         this.weight=w;
@@ -30,13 +38,31 @@ public class Excercise {
         this.reps=r;
         this.weight=defaultweight; // we set weight as default weight value
     }
-    public int getRep(){
-        return this.reps;
+    public Excercise(String n){
+        this.excerciseName=n; //we set name to default name
+    }
+    public String getName(){
+        System.out.print("Enter the name of this exercise: ");
+        excerciseName = myScanner.next();
+        return this.excerciseName;
     }
     public int getSet(){
+        System.out.print("Enter the number of sets in this exercise: ");
+        sets = myScanner.nextInt();
         return this.sets;
     }
+    public int getRep(){
+        System.out.print("Enter the number of reps in this exercise: ");
+        reps = myScanner.nextInt();
+        return this.reps;
+    }
     public double getWeight(){
+        System.out.print("Enter the target weight in this exercise: ");
+        weight = myScanner.nextInt();
         return this.weight;
+    }
+    public String[] returnExercise(){
+        String [] exercise = {excerciseName, String.valueOf(reps), String.valueOf(sets), String.valueOf(weight)};
+        return exercise;
     }
 }
