0330eaed966c3f01bc0d5b0e42777aa3106bf7d2
sebstirs <98188443+sebstirs@users.noreply.github.com>
2022-03-29 16:53:47 -0400

    Add files via upload

---
+++ BeginnerWorkouts.java
@@ -0,0 +1,13 @@
+public class BeginnerWorkouts {
+
+    public void PushChest(){
+
+        System.out.println("Flat Bench: 5x5  90s rest");
+        System.out.println("Shoulder Press: 3x25  60s rest");
+        System.out.println("Dips: 3 x max  60s rest");
+        System.out.println("Tricep pushdowns: 3x25  30s rest");
+        System.out.println("DB lateral raise: 3 x 20  30s rest");
+
+    }
+    
+}
