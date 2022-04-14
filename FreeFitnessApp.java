5400f074293e227ada37b455b36db97a63788d06
sebstirs <98188443+sebstirs@users.noreply.github.com>
2022-04-14 12:34:35 -0400

    Delete FreeFitnessApp.java

--- FreeFitnessApp.java
+++
@@ -1,9 +0,0 @@
-public class FreeFitnessApp {
-
-	public static void main(String[] args) {
-		Workouts myNewWorkOut = new Workouts();
-
-		myNewWorkOut.getNumExercise();
-		myNewWorkOut.exerciseList();
-	}
-}
