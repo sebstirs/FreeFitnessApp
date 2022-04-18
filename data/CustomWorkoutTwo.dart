ab9524af9ae5f3795c6c58e112924c2276e98289
sebstirs <98188443+sebstirs@users.noreply.github.com>
2022-04-18 13:26:15 -0400

    Add files via upload

---
+++ data/CustomWorkoutTwo.dart
@@ -0,0 +1,14 @@
+import 'package:myapp/model/user.dart';
+
+final WorkoutTwo = <User>[
+  User(Exercise: 'Chest Bench Press', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+  User(Exercise: 'Enter Exercise', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+  User(Exercise: 'Enter Exercise', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+  User(Exercise: 'Enter Exercise', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+  User(Exercise: 'Enter Exercise', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+  User(Exercise: 'Enter Exercise', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+  User(Exercise: 'Enter Exercise', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+  User(Exercise: 'Enter Exercise', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+  User(Exercise: 'Enter Exercise', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+  User(Exercise: 'Enter Exercise', Sets: 'Enter Sets', Reps: 'Enter Reps'),
+];
